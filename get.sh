09f3a402955da3e95f33331389988a84ea855f55
Alex Ellis <alexellis2@gmail.com>
2019-08-19 08:28:38 +0100

    Add generic build script
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- get.sh
+++ get.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 # This script was adapted from https://github.com/openfaas/cli.openfaas.com/blob/master/get.sh
 
-version=$(curl -sI https://github.com/alexellis/inlets/releases/latest | grep Location | awk -F"/" '{ printf "%s", $NF }' | tr -d '\r')
+export OWNER=alexellis
+export REPO=inlets
+export SUCCESS_CMD="$REPO --version"
+
+version=$(curl -sI https://github.com/$OWNER/$REPO/releases/latest | grep Location | awk -F"/" '{ printf "%s", $NF }' | tr -d '\r')
 
 if [ ! $version ]; then
-    echo "Failed while attempting to install inlets. Please manually install:"
+    echo "Failed while attempting to install $REPO. Please manually install:"
     echo ""
-    echo "1. Open your web browser and go to https://github.com/alexellis/inlets/releases"
-    echo "2. Download the latest release for your platform. Call it 'inlets'."
-    echo "3. chmod +x ./inlets"
-    echo "4. mv ./inlets /usr/local/bin"
+    echo "1. Open your web browser and go to https://github.com/$OWNER/$REPO/releases"
+    echo "2. Download the latest release for your platform. Call it '$REPO'."
+    echo "3. chmod +x ./$REPO"
+    echo "4. mv ./$REPO /usr/local/bin"
     exit 1
 fi
 
 hasCli() {
 
-    has=$(which inlets)
+    has=$(which $REPO)
 
     if [ "$?" = "0" ]; then
         echo
-        echo "You already have the inlets cli!"
+        echo "You already have the $REPO cli!"
         export n=1
         echo "Overwriting in $n seconds.. Press Control+C to cancel."
         echo
@@ -58,20 +62,20 @@ getPackage() {
     ;;
     esac
 
-    targetFile="/tmp/inlets$suffix"
+    targetFile="/tmp/$REPO$suffix"
 
     if [ "$userid" != "0" ]; then
-        targetFile="$(pwd)/inlets$suffix"
+        targetFile="$(pwd)/$REPO$suffix"
     fi
 
     if [ -e $targetFile ]; then
         rm $targetFile
     fi
 
-    url=https://github.com/alexellis/inlets/releases/download/$version/inlets$suffix
+    url=https://github.com/alexellis/$REPO/releases/download/$version/$REPO$suffix
     echo "Downloading package $url as $targetFile"
 
-    curl -sSL $url --output $targetFile
+    curl -sSLf $url --output $targetFile
 
     if [ "$?" = "0" ]; then
 
@@ -87,25 +91,25 @@ getPackage() {
             echo "==    following commands may need to be run manually   =="
             echo "========================================================="
             echo
-            echo "  sudo cp inlets$suffix /usr/local/bin/inlets"
+            echo "  sudo cp $REPO$suffix /usr/local/bin/$REPO"
             echo
-
+            ./$REPO$suffix --version
         else
 
             echo
-            echo "Running as root - Attempting to move inlets to /usr/local/bin"
+            echo "Running as root - Attempting to move $REPO to /usr/local/bin"
 
-            mv $targetFile /usr/local/bin/inlets
+            mv $targetFile /usr/local/bin/$REPO
 
             if [ "$?" = "0" ]; then
-                echo "New version of inlets installed to /usr/local/bin"
+                echo "New version of $REPO installed to /usr/local/bin"
             fi
 
             if [ -e $targetFile ]; then
                 rm $targetFile
             fi
 
-           inlets --version
+           ${SUCCESS_CMD}
         fi
     fi
 }
